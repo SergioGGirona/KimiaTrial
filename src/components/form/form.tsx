2aff1ac5dc933e02c2782d82e92ccea3544b1798
Sergio G. Girona <sergioglez_8@hotmail.com>
2023-12-28 13:56:46 +0100

    Add step form 2 and functionality

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,51 +1,63 @@
-import houseIcon from "../../assets/imagenes/casa.svg";
-import flatIcon from "../../assets/imagenes/piso.svg";
-
+import { FormEvent, useState } from "react";
+import { useMultiStepForm } from "../hooks/useMultistep";
 import styles from "./form.module.scss";
+import { Form1 } from "./form.step1/form.step1";
+import { Form2 } from "./form.step2/form.step2";
+
+type FormData = {
+  placeToInstall: string;
+  name: string;
+  surname: string;
+  email: string;
+  phone: number;
+  isChecked: boolean;
+};
+
+const initialData: FormData = {
+  placeToInstall: "",
+  name: "",
+  surname: "",
+  email: "",
+  phone: 0,
+  isChecked: false,
+};
 export function Form() {
-  const handleIconClick = (id: string) => {
-    const inputElement = document.getElementById(id);
-    if (inputElement) {
-      inputElement.click();
-    }
+  const [data, setData] = useState(initialData);
+  function updateFields(fields: Partial<FormData>) {
+    setData((prev) => {
+      return { ...prev, ...fields };
+    });
+  }
+  const { step, nextStep, currentStepIndex } = useMultiStepForm([
+    <Form1 {...data} updateFields={updateFields} />,
+    <Form2 {...data} updateFields={updateFields} />,
+  ]);
+
+  const onSubmit = (ev: FormEvent) => {
+    ev.preventDefault();
+    console.log(data);
   };
   return (
-    <div className={styles.form}>
+    <form className={styles.form} onSubmit={onSubmit}>
       <hgroup>
         <h1>Encuentra al mejor instalador de paneles solares en tu zona</h1>
         <p>Calcula tu ahorro en menos de 1 minuto</p>
       </hgroup>
       <i className="fa-solid fa-chevron-down"></i>
-      <h2>¿Dónde quieres instalar tus placas solares?</h2>
-      <div>
-        <div className={styles.icon}>
-          <img
-            src={houseIcon}
-            alt="Un sencillo icono de una casa con un tejado rojo"
-            onClick={() => handleIconClick("house")}
-          />
-          <input type="radio" name="placeToInstall" id="house" />
-          <label htmlFor="house">Casa</label>
-        </div>
-        <div className={styles.icon}>
-          <img
-            src={flatIcon}
-            alt="Un sencillo icono de un piso moderno"
-            onClick={() => handleIconClick("flat")}
-          />
-          <input type="radio" name="placeToInstall" id="flat" />
-          <label htmlFor="flat">Bloque de pisos</label>
-        </div>
 
-        <div className={styles.stepBar}>
-          <div className={styles.bar}></div>
-          <p>1 de 2</p>
-        </div>
-      </div>
-      <a className={styles.next}>
-        <p>Siguiente</p>
-        <i className="fa-solid fa-arrow-right"></i>
-      </a>
-    </div>
+      {step}
+
+      {currentStepIndex === 0 && (
+        <a className={styles.next} onClick={nextStep}>
+          Siguiente
+          <i className="fa-solid fa-arrow-right"></i>
+        </a>
+      )}
+      {currentStepIndex === 1 && (
+        <button className={styles.endForm} type="submit">
+          Finalizar
+        </button>
+      )}
+    </form>
   );
 }
